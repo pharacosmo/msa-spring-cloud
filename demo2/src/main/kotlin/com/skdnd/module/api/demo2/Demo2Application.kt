9813965443aa8d11258930a5753d63f87a59c7d1
gamars <gamars80@gmail.com>
2025-03-06 15:40:47 +0900

    demo1/2

--- demo2/src/main/kotlin/com/skdnd/module/api/demo2/Demo2Application.kt
+++ demo2/src/main/kotlin/com/skdnd/module/api/demo2/Demo2Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient
 
-@SpringBootApplication
 @EnableDiscoveryClient
+@SpringBootApplication
 class Demo2Application
 
 fun main(args: Array<String>) {
