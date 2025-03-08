505ca6f4e6a41457576802cc60b67a14bf3edec9
paracosmos <iwillbebback@gmail.com>
2025-03-08 15:16:47 +0900

    get test

--- demo/src/main/kotlin/com/example/demo/DemoController.kt
+++ demo/src/main/kotlin/com/example/demo/DemoController.kt
@@ -1,7 +1,10 @@
 package com.example.demo
 
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestHeader
 import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
@@ -9,4 +12,19 @@ import org.springframework.web.bind.annotation.RestController
 class DemoController {
     @GetMapping
     fun hello() = "demo1"
+
+    @GetMapping("/query")
+    fun getQueryParam(@RequestParam name: String): String {
+        return "Received query parameter: $name"
+    }
+
+    @GetMapping("/path/{id}")
+    fun getPathParam(@PathVariable id: Int): String {
+        return "Received path parameter: $id"
+    }
+
+    @GetMapping("/header")
+    fun getHeader(@RequestHeader("User-Agent") userAgent: String): String {
+        return "Received header User-Agent: $userAgent"
+    }
 }
